de7ef748ac6d4196f20baf693e11e728888972d2
5anchezzz <5an4e3.free@gmail.com>
2020-11-12 17:20:54 +0300

    feat: remove case sensitive from email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   validates :firstname, presence: true
   #validates :lastname, presence: true
   validates :language, presence: true
-  validates :email, uniqueness: true
+  validates :email, uniqueness: { case_sensitive: false }
 
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
 
