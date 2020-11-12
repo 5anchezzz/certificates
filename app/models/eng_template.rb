e7cdd9ff9b3ccb7e6d4e3582cc0c5295eaccac8e
5anchezzz <5an4e3.free@gmail.com>
2020-11-12 18:56:58 +0300

    feat: add rgb code format validation

--- app/models/eng_template.rb
+++ app/models/eng_template.rb
@@ -5,6 +5,7 @@ class EngTemplate < ApplicationRecord
   validates :ypos, presence: true, numericality: { only_integer: true }
   validates :font_size, presence: true, numericality: { only_integer: true }
   validates :font_color, presence: true
+  validates_format_of :font_color, with: /\A^([0-1]?\d?\d|2[0-4]\d|25[0-5]),([0-1]?\d?\d|2[0-4]\d|25[0-5]),([0-1]?\d?\d|2[0-4]\d|25[0-5])$\z/i, on: [:create, :update]
 
   has_attached_file :pdf_file, styles: { :thumb => ["200x200>", :png], :medium => ["500x500>", :png] }
   validates_attachment :pdf_file, presence: true,
