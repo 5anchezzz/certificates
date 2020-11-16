d188ae95b674ca009b47bfe41ece83e9a772e984
5anchezzz <5an4e3.free@gmail.com>
2020-11-16 12:23:50 +0300

    refactor: add set name method and new generate cert with prawn

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
 
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
 
+  before_save :set_name_width
+
   def name_to_paste
     firstname.upcase + (" #{lastname.upcase}" if lastname).to_s
   end
@@ -43,6 +45,33 @@ class User < ApplicationRecord
   #end
 
 
+  def generate_cert_with_prawn(certificate)
+    language == 'rus' ? using_template = certificate.rus_template : using_template = certificate.eng_template
+    color = using_template.font_color.split(',').map(&:to_i)
+    start_pos = using_template.xpos - ((name_width * using_template.font_size * 0.95 / 15) / 2)
 
+    prawn_text_pdf = Prawn::Document.new :page_size => [using_template.pdf_height, using_template.pdf_width], :margin => 0 do |pdf|
+      pdf.fill_color color[0], color[1], color[2], color[3]
+      pdf.font Rails.root.join('public','Montserrat-Medium.ttf')
+      pdf.draw_text name_to_paste, :at => [start_pos, using_template.ypos], :size => using_template.font_size * 0.7
+    end
+
+    pdf_data = prawn_text_pdf.render
+    combine_text_pdf = CombinePDF.parse(pdf_data).pages[0]
+
+    cert_pdf = CombinePDF.load using_template.pdf_file.path
+    cert_pdf.pages[0] << combine_text_pdf
+    cert_pdf.to_pdf
+
+  end
+
+  private
+
+  def set_name_width
+    label = Magick::Draw.new
+    label.font Rails.root.join('public','Montserrat-Medium.ttf')
+    width = label.get_type_metrics(name_to_paste).width
+    self.name_width = width if self.name_width != width
+  end
 
 end
