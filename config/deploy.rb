01da3d7389a023238c7af1031245bb7ec129847c
5anchezzz <5an4e3.free@gmail.com>
2021-04-20 12:49:58 +0300

    add sidekiq sets to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -140,3 +140,44 @@ end
 # kill -s SIGUSR2 pid   # Restart puma
 # kill -s SIGTERM pid   # Stop puma
 #for PUMA<<<
+
+#####
+set :sidekiq_roles, -> { :app }
+set :sidekiq_systemd_unit_name, "sidekiq@#{fetch(:stage)}"
+
+namespace :sidekiq do
+  desc 'Quiet sidekiq (stop fetching new tasks from Redis)'
+  task :quiet do
+    on roles fetch(:sidekiq_roles) do
+      # See: https://github.com/mperham/sidekiq/wiki/Signals#tstp
+      execute :systemctl, '--user', 'kill', '-s', 'SIGTSTP', fetch(:sidekiq_systemd_unit_name), raise_on_non_zero_exit: false
+    end
+  end
+
+  desc 'Stop sidekiq (graceful shutdown within timeout, put unfinished tasks back to Redis)'
+  task :stop do
+    on roles fetch(:sidekiq_roles) do
+      # See: https://github.com/mperham/sidekiq/wiki/Signals#tstp
+      execute :systemctl, '--user', 'kill', '-s', 'SIGTERM', fetch(:sidekiq_systemd_unit_name), raise_on_non_zero_exit: false
+    end
+  end
+
+  desc 'Start sidekiq'
+  task :start do
+    on roles fetch(:sidekiq_roles) do
+      execute :systemctl, '--user', 'start', fetch(:sidekiq_systemd_unit_name)
+    end
+  end
+
+  desc 'Restart sidekiq'
+  task :restart do
+    on roles fetch(:sidekiq_roles) do
+      execute :systemctl, '--user', 'restart', fetch(:sidekiq_systemd_unit_name)
+    end
+  end
+end
+
+after 'deploy:starting', 'sidekiq:quiet'
+after 'deploy:updated', 'sidekiq:stop'
+after 'deploy:published', 'sidekiq:start'
+after 'deploy:failed', 'sidekiq:restart'
